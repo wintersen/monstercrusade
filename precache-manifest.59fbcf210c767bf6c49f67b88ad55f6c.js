b020124d74e47193e47b4bce87f01e98e2da2755
Erika <black95@me.com>
2021-02-09 14:52:51 -0500

    Updates

---
+++ precache-manifest.59fbcf210c767bf6c49f67b88ad55f6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8acbf5aa99be8a3f560ab29fe51077",
+    "url": "/monstercrusade/index.html"
+  },
+  {
+    "revision": "0c7ac88c15707ebb5723",
+    "url": "/monstercrusade/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ccef1cdea019225613c8",
+    "url": "/monstercrusade/static/css/main.7a665ef4.chunk.css"
+  },
+  {
+    "revision": "0c7ac88c15707ebb5723",
+    "url": "/monstercrusade/static/js/2.70b8cf63.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/monstercrusade/static/js/2.70b8cf63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccef1cdea019225613c8",
+    "url": "/monstercrusade/static/js/main.3e85c54c.chunk.js"
+  },
+  {
+    "revision": "1a23db8ffd57e6690553",
+    "url": "/monstercrusade/static/js/runtime-main.61a4ad55.js"
+  },
+  {
+    "revision": "8d4647c9d133b33a2d04735960985461",
+    "url": "/monstercrusade/static/media/bg.8d4647c9.png"
+  },
+  {
+    "revision": "e59082fee81d4af4250560c554ee90e9",
+    "url": "/monstercrusade/static/media/floor.e59082fe.png"
+  },
+  {
+    "revision": "8f94cab37c2f2b0e1ce8f07c5ce9f619",
+    "url": "/monstercrusade/static/media/temp2.8f94cab3.png"
+  }
+]);
